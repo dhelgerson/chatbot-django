865e61e5082bd64e25d9862714af3906617c3ee9
Drew Helgerson <tah568@msstate.edu>
2023-11-27 12:27:57 -0600

    mhm

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# chat/views.py
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+class ChatView(TemplateView):
+    template_name: str = "chat/chat.html"
